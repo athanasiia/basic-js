f994614a60c8ee63d75773b4a57d929e17db3762
athanasiia <grvkan@gmail.com>
2023-12-19 22:47:35 +0300

    feat: complete tasks 17-19

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let exceptionIndex = [];
+
+  arr.forEach((element, i) => {
+    if (element === -1) {
+      exceptionIndex.push(i);
+    }
+  })
+
+  arr = arr.sort((a, b) => a - b).splice(exceptionIndex.length);
+
+  exceptionIndex.forEach(index => {
+    arr.splice(index, 0, -1);
+  })
+
+  return arr;
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+
+  let newArr = [];
+  let shouldBeDiscarded = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (shouldBeDiscarded) {
+      newArr.push(undefined);
+      shouldBeDiscarded = false;
+      continue;
+    }
+
+    if (arr[i] === '--discard-next') shouldBeDiscarded = true;
+    else if (arr[i] === '--discard-prev') newArr.pop();
+    else if (arr[i] === '--double-next') newArr.push(arr[i + 1]);
+    else if (arr[i] === '--double-prev') newArr.push(newArr[newArr.length - 1]);
+    else newArr.push(arr[i]);
+  }
+
+  newArr = newArr.filter(element => element !== undefined);
+
+  return newArr;
 }
 
 module.exports = {
