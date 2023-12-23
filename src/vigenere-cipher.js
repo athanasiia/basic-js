b79e2cf07bd7ee7a1b9fb68ba6285a613ee4a149
athanasiia <grvkan@gmail.com>
2023-12-23 18:14:10 +0300

    feat: complete tasks 20-21

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(notInverted = true) {
+    this.notInverted = notInverted;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    const keys = key.toUpperCase().split('').map(letter => alphabet.indexOf(letter));
+    let keyIndex = 0;
+    message = message.toUpperCase().split('');
+
+    let encryptedMessage = [];
+    let encryptedLetter;
+
+    for (let i = 0; i < message.length; i++) {
+    if (!alphabet.includes(message[i])) {
+      encryptedMessage.push(message[i]);
+      continue;
+    }    
+    encryptedLetter = alphabet[alphabet.indexOf(message[i]) + keys[keyIndex]];
+    encryptedMessage.push(encryptedLetter);
+    keyIndex = keyIndex == keys.length - 1 ? 0 : keyIndex + 1;
+  }
+
+    return this.notInverted ? encryptedMessage.join('') : encryptedMessage.reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    const keys = key.toUpperCase().split('').map(letter => alphabet.indexOf(letter));
+    let keyIndex = 0;
+    encryptedMessage = encryptedMessage.toUpperCase().split('');
+
+    let message = [];
+    let letter;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+    if (!alphabet.includes(encryptedMessage[i])) {
+      message.push(encryptedMessage[i]);
+      continue;
+    }    
+    letter = alphabet[alphabet.lastIndexOf(encryptedMessage[i]) - keys[keyIndex]];
+    message.push(letter);
+    keyIndex = keyIndex == keys.length - 1 ? 0 : keyIndex + 1;
+  }
+
+    return this.notInverted ? message.join('') : message.reverse().join('');
   }
 }
 
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function checkIfLeapYear(year) {
+  if (year % 4 !== 0) return false;
+  if (year % 100 === 0 && year % 400 !== 0) return false;
+  return true;
+}
+
+function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  if (!(date instanceof Date)) throw new Error('Invalid date!');
+  if (date[Symbol.toStringTag]) throw new Error('Invalid date!');
+
+  let listOfDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (checkIfLeapYear(date.getFullYear())) {
+    listOfDays[1] = 29;
+  }
+
+  if (date.getDate() <= 0 || date.getDate() > listOfDays[date.getMonth()] ||
+  date.getMonth() < 0 || date.getMonth() > 11) throw new Error('Invalid date!');
+
+  let yearFromString = date.toString().split('').slice(11, 15).join('');
+  if (yearFromString != date.getFullYear()) throw new Error('Invalid date!');
+
+  if (date.getMonth() >= 2 && date.getMonth() < 5) return 'spring';
+  if (date.getMonth() >= 5 && date.getMonth() < 8) return 'summer';
+  if (date.getMonth() >= 8 && date.getMonth() < 11) return 'fall';
+  return 'winter';
 }
 
 module.exports = {
